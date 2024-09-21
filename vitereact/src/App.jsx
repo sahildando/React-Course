81f00ec7aca1dbeb761949449181a318622bcf12
SAHIL SHARMA <118820911+sahildando@users.noreply.github.com>
2024-09-21 20:15:47 +0530

    React and Jsx lib difference

--- vitereact/src/App.jsx
+++ vitereact/src/App.jsx
@@ -1,13 +1,15 @@
 import Sahil from "./sahil"
 
 function App() {
+  const username = "  Sahil the Undisputed"
+  
  
 
   return (
     <div> 
       <Sahil/>
       <h1>
-        Sahil working on React
+        Sahil working on React{username}
       </h1>
       <p>
         Testing Deployment on Jenkins
--- vitereact/src/main.jsx
+++ vitereact/src/main.jsx
@@ -1,10 +1,44 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp(){
+    return(
+        <div>
+            <h1>Custom Application | SAHIL</h1>
+        </div>
+    )
+}
+// const ReactElement = {
+//     type: 'a',
+//     props: {
+//         href: 'https://google.com',
+//         target: '_blank'
+//     },
+//     children: 'Click me to visit google'
+// }
 
-createRoot(document.getElementById('root')).render(
+const anotherElement = (
+    <a href="https://google.com" target='_blank'>Visit Google</a>
+
+)
+
+const anotherUser = "  React with Js mastery"
+
+const reactElement = React.createElement(
+    'a',
+    {href: 'https://google.com', target: '_blank' },
+    'click me to visit google',
+    anotherUser
+
+)
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+
+    reactElement
   
-    <App />
+   
  
 )
