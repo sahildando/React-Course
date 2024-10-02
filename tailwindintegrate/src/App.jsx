cbb9d9b088a67f06e4f3aac3d0098b8f8f89cd56
SAHIL SHARMA <118820911+sahildando@users.noreply.github.com>
2024-10-02 09:55:07 +0530

    push

--- tailwindintegrate/src/App.jsx
+++ tailwindintegrate/src/App.jsx
@@ -2,14 +2,25 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './components/Card'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  let myObj = {
+    username: "Sahil",
+    age: 20
+  }
+
+  let newArr = [1,2,3]
+
   return (
     <>
- 
- 
+ <h1 className='bg-green-400 text black p-4 rounded-xl mb-4'>Tailwind Configuration</h1>
+ <Card channel="Sahil React Series" someObje={myObj} />
+ <br />
+ <Card />
+
 
     </>
   )
